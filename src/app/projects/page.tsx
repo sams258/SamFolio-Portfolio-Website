30e2be2f17ff6eef5593973e8f342ab02645a147
Sam <samsse46@gmail.com>
2025-01-18 22:29:00 +0100

    quick fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+"use client";
+
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const ProjectsPage: React.FC = () => {
   return (
@@ -8,7 +10,9 @@ const ProjectsPage: React.FC = () => {
       <Header />
       <main className="py-16 container mx-auto">
         <h1 className="text-3xl font-bold mb-6">More Projects</h1>
-        <p className="text-lg text-gray-700">Detailed information about my projects...</p>
+        <p className="text-lg text-gray-700">
+          Detailed information about my projects...
+        </p>
       </main>
       <Footer />
     </>
