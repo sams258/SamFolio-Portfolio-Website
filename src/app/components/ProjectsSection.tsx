0630bb4bc49c86762b88faaa541a810451b885fd
Sam <115927233+sams258@users.noreply.github.com>
2025-02-14 11:48:10 +0100

    Update ProjectsSection.tsx

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -108,16 +108,12 @@ const ProjectsSection: React.FC = () => {
         </div>
         {/* More Projects Button */}
         <div className="flex justify-center">
-          <button className="glassnav w-50 mt-6 md:mt-6 mb-6 hover:bg-transparent flex justify-center">
-            <div className="text-center mt-2 mb-2">
-              <Link
-                href="/github"
-                className="text-teal-500 font-medium text-lg hover:text-teal-600 mt-2"
-              >
-                More projects...
-              </Link>
-            </div>
-          </button>
+          <a
+  href="/github"
+  className="glassnav w-50 mt-6 md:mt-6 mb-6 hover:bg-transparent flex justify-center text-teal-500 font-medium text-lg hover:text-teal-600 text-center py-2"
+>
+  More projects...
+           </a>
         </div>
       </div>
     </motion.section>
