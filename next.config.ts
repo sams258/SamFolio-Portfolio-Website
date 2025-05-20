ddf924846bf8899943af17beafa8f60e62df9740
Sam <samsse46@gmail.com>
2025-05-20 15:01:58 +0200

    updated CV

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/samscv.pdf",
+        source: "/samscv0525.pdf",
         headers: [
           {
             key: "X-Robots-Tag",
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -31,8 +31,8 @@ const HeroSection: React.FC = () => {
               View My Work
             </a>
             <a
-              href="/samscv.pdf"
-              download="samscv.pdf"
+              href="/samscv0525.pdf"
+              download="samscv0525.pdf"
               className="px-6 py-3 bg-white border-2 border-orange-400 text-orange-400 font-bold rounded-lg shadow-md hover:bg-orange-400 hover:text-white hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-orange-200"
             >
               Download CV
